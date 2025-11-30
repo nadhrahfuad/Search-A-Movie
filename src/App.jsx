78a5d4f8423db33862de7e7e6beca3d8cd6cf427
nadhrahfuad <nadhrah.fuad@gmail.com>
2025-11-30 20:13:08 +0800

    added cart on display

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import "./styles/app.css"
 import Searchbar from './components/Searchbar'
 import Display from './components/Display'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBookmark } from '@fortawesome/free-solid-svg-icons'
-import Cart from './components/Cart'
-import { useContext } from 'react'
-import CartContext from './context/CartContext'
+import { faBookmark, faShoppingBag} from '@fortawesome/free-solid-svg-icons'
+import Saved from './components/Saved'
+import { useContext, useEffect, useState } from 'react'
+import SavedContext from './context/SavedContext'
 import MovieLogo from "./assets/13-26-07-593_512.webp"
+import Cart from "./components/Cart"
+import CartContext from "./context/CartContext"
 
 
 
@@ -17,7 +19,8 @@ import MovieLogo from "./assets/13-26-07-593_512.webp"
 function App() {
 
 
-  const {openList, cartCount, toggleList} = useContext(CartContext)
+  const {openList, savedCount, toggleList} = useContext(SavedContext)
+  const {openCart, cartCount, toggleCart} = useContext(CartContext)
 
 
 
@@ -31,13 +34,19 @@ function App() {
       <img className="imglogo" src={MovieLogo} alt="" />
       </div>
 
-    <div className="searchNCart flex">
+    <div className="searchNSave flex">
         <Searchbar />
     
-    <div className="carticoncontainer">
+    <div className="savediconcontainer">
 
-    <span className= {`cartnum txtlight`}>{cartCount}</span>
-    <FontAwesomeIcon className={`${cartCount > 0? "primary" : "txtlight"}`}  onClick={toggleList} icon={faBookmark} />
+    <span className= {`savednum txtlight`}>{savedCount}</span>
+    <FontAwesomeIcon className={`${savedCount > 0? "primary" : "txtlight"}`}  onClick={toggleList} icon={faBookmark} />
+
+    </div>
+     <div className="savediconcontainer">
+
+    <span className= {`savednum txtlight`}>{cartCount}</span>
+    <FontAwesomeIcon className={`${cartCount > 0? "primary" : "txtlight"}`}  onClick={toggleCart} icon={faShoppingBag} />
 
     </div>
     </div>
@@ -45,10 +54,18 @@ function App() {
 
     {
       openList &&
-      <Cart />
+      <Saved />
 
     }
      
+     {
+      openCart && 
+      <Cart
+
+
+      
+      />
+     }
 
    
   
