db67329cd8f61ff80a44286fb6d35d4fcb975d2e
nadhrahfuad <nadhrah.fuad@gmail.com>
2025-11-30 20:13:19 +0800

    fixed context wrapping

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import MovieContextProvider from './context/MovieContextProvider.jsx'
-import CartContextProvider from './context/CartContextProvider.jsx'
+import SavedContextProvider from './context/SavedContextProvider.jsx'
 import "./styles/main.css"
+import CartContextProvider from './context/CartContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <MovieContextProvider>
+      <SavedContextProvider>
       <CartContextProvider>
-    <App />
-    </CartContextProvider>
+        
+          <App />
+          </CartContextProvider>
+        </SavedContextProvider>
+      
     </MovieContextProvider>
   </StrictMode>,
 )
